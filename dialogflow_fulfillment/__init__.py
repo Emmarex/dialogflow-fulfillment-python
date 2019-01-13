3b37c44930f927450a10487be4aac865dd07337d
Emmarex <tairuoluwafemi09@gmail.com>
2019-01-13 13:03:02 +0100

    modified setup.py

--- dialogflow_fulfillment/__init__.py
+++ dialogflow_fulfillment/__init__.py
@@ -1,3 +1,5 @@
+name = "dialogflow_fulfillment"
+
 from dialogflow_request import DialogflowRequest
 from dialogflow_response import DialogflowResponse
 from response import SimpleResponse, Suggestions, SystemIntent
--- setup.py
+++ setup.py
@@ -4,19 +4,19 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='',  
-    version='0.1',
-    scripts=[''],
-    author="",
-    author_email="",
-    description="",
+    name='dialogflow_fulfillment',  
+    version='0.0.1',
+    author="Tairu Oluwafemi Emmanuel",
+    author_email="developer.emmarex@gmail.com",
+    description="This Libray makes creating fulfillment for Dialogflow v2 agents with Django or Flask",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/Emmarex/dialogflow-fulfillment-python",
     packages=setuptools.find_packages(),
     classifiers=[
-         "Programming Language :: Python :: 3",
-         "License :: OSI Approved :: MIT License",
-         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 3 - Alpha"
+        "Operating System :: OS Independent",
      ],
  )
