70e1832c3e8ea456c153bbb90698ba0371aea1d7
Emmarex <tairuoluwafemi09@gmail.com>
2019-01-14 05:51:18 +0100

    Modified README.md

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 The <a href="">Dialogflow Fulfillment Library</a> allows you to connect natural language understanding and processing to your own systems, APIs, and databases. Using Fulfillment, you can surface commands and information from your services to your users through a natural conversational interface.
 
-This Libray makes creating fulfillment for Dialogflow v2 agents with <a href="">Django</a> or <a href="">Flask</a> easy and simple.
+This Library makes creating fulfillment for Dialogflow v2 agents with <a href="">Django</a> or <a href="">Flask</a> easy and simple.
 
 ## Supported Features
 
--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ setuptools.setup(
     version='0.0.1',
     author="Tairu Oluwafemi Emmanuel",
     author_email="developer.emmarex@gmail.com",
-    description="This Libray makes creating fulfillment for Dialogflow v2 agents with Django or Flask easy and simple",
+    description="This Library makes creating fulfillment for Dialogflow v2 agents with Django or Flask easy and simple",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Emmarex/dialogflow-fulfillment-python",
     packages=setuptools.find_packages(),
-    classifiers=[
+    classifiers=[ 
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Intended Audience :: Developers",
