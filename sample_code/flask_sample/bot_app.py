954d14e38d0b60b54af65a4b9fba4331a179f789
Emmarex <tairuoluwafemi09@gmail.com>
2019-06-02 15:01:51 +0100

    updated sample code

--- sample_code/flask_sample/bot_app.py
+++ sample_code/flask_sample/bot_app.py
@@ -6,15 +6,19 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index_page():
     if request.method == 'POST':
+        print('post')
         dialogflow_request = DialogflowRequest(request.data)
         if dialogflow_request.get_intent_displayName() == "welcome_intent":
             dialogflow_response = DialogflowResponse("Welcome to my test dialogflow webhook")
             dialogflow_response.add(SimpleResponse("Welcome to my test dialogflow webhook","Welcome to my test dialogflow webhook"))
             response = jsonify(dialogflow_response.get_final_response())
         else:
-            dialogflow_response = DialogflowResponse("Now that you are here. What can I help you with ?")
+            dialogflow_response = DialogflowResponse()
+            dialogflow_response.add(SimpleResponse("Welcome to my test dialogflow webhook","Welcome to my test dialogflow webhook"))
             dialogflow_response.add(Suggestions(["About","Sync","More info"]))
             response = jsonify(dialogflow_response.get_final_response())
+            print(dialogflow_response.get_final_response())
         return response
     else:
+        print('404')
         abort(404)
