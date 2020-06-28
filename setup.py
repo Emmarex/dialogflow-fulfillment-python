e80b8630bc7daa455b499f55f67a4c24db65d434
Emmarex <tairuoluwafemi09@gmail.com>
2020-06-28 13:55:14 +0100

    updated version number

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='pydialogflow_fulfillment',  
-    version='0.1.3',
+    version='0.1.4',
     author="Tairu Oluwafemi Emmanuel",
     author_email="developer.emmarex@gmail.com",
     description="This Library makes creating fulfillment for Dialogflow v2 agents with Django or Flask easy and simple",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Emmarex/dialogflow-fulfillment-python",
-    download_url="https://github.com/Emmarex/dialogflow-fulfillment-python/archive/0.1.0.tar.gz",
+    download_url="https://github.com/Emmarex/dialogflow-fulfillment-python/archive/0.1.4.tar.gz",
     keywords= ['Dialogflow', 'Dialogflow fulfillment', 'Dialogflow fulfillment v2', 'pydialogflow_fulfillment'],
     packages=setuptools.find_packages(),
     classifiers=[ 
